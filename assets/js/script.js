2c4e70967622e7d12d16dd593c120a2137ba56bd
utkarshbhardwaj01 <67866657+utkarshbhardwaj01@users.noreply.github.com>
2021-12-21 17:22:40 +0530

    added dynamic + and - of tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,9 +35,57 @@ window.addEventListener('load', () => {
 
 })
 
-// Remove Item Module
+// // Remove Item Module
+//
+// var close = document.getElementsByClassName("remove");
+// var i;
+// for (i = 0; i < close.length; i++) {
+//   close[i].onclick = function() {
+//     var div = this.parentElement;
+//     div.style.display = "none";
+//   }
+// }
+//
+// // Create a new list item when clicking on the "Add" button
+// function addTask() {
+//   var li = document.createElement("input");
+//   var inputValue = document.getElementById("addTask").value;
+//   var t = document.createTextNode(inputValue);
+//   li.appendChild(t);
+//   if (inputValue === '') {
+//     alert("You must write something!");
+//   } else {
+//     document.getElementById("list-group").appendChild(li);
+//   }
+//   document.getElementById("addTask").value = "";
+//
+//   var span = document.createElement("SPAN");
+//   var txt = document.createTextNode("\u00D7");
+//   span.className = "close";
+//   span.appendChild(txt);
+//   li.appendChild(span);
+//
+//   for (i = 0; i < close.length; i++) {
+//     close[i].onclick = function() {
+//       var div = this.parentElement;
+//       div.style.display = "none";
+//     }
+//   }
+// }
 
-var close = document.getElementsByClassName("remove");
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("button");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "remove btn btn-danger";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("remove btn btn-danger");
 var i;
 for (i = 0; i < close.length; i++) {
   close[i].onclick = function() {
@@ -47,21 +95,23 @@ for (i = 0; i < close.length; i++) {
 }
 
 // Create a new list item when clicking on the "Add" button
-function addTask() {
+function newElement() {
   var li = document.createElement("li");
+  li.className = "list-group-item";
+  li.setAttribute("checkbox", "text");
   var inputValue = document.getElementById("myInput").value;
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
   if (inputValue === '') {
-    alert("You must write something!");
+    alert("Please add an item");
   } else {
     document.getElementById("myUL").appendChild(li);
   }
   document.getElementById("myInput").value = "";
 
-  var span = document.createElement("SPAN");
+  var span = document.createElement("Button");
   var txt = document.createTextNode("\u00D7");
-  span.className = "close";
+  span.className = "remove btn btn-danger";
   span.appendChild(txt);
   li.appendChild(span);
 
